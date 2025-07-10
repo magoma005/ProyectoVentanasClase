0da2cc27074511cb3d154c0c54310b1da24223e3
Miguel Angel Guarin <mago123005@gmail.com>
2025-07-10 18:30:57 -0500

    Edicion en la imagen

--- src/VentanaSecundaria.java
+++ src/VentanaSecundaria.java
@@ -1,19 +1,27 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class VentanaSecundaria extends JFrame {
 
     public VentanaSecundaria() {
         setTitle("ANUNCIO VE A VER SUPERMAN");
 
-        // Mostrar mensaje al abrirse
         JOptionPane.showMessageDialog(this, "VE A VER SUPERMAN ESTE 8 DE JULIO");
+
+        // Establecer layout general
+        setLayout(new BorderLayout());
 
         // Crear un JLabel con la imagen
         ImageIcon imagen = new ImageIcon(getClass().getResource("/imagenes/12.jpg"));
         JLabel labelImagen = new JLabel(imagen);
+        add(labelImagen, BorderLayout.CENTER);
+        //Boton Ok de la imagen
+        JButton btnOK = new JButton("OK");
+        btnOK.addActionListener(e -> dispose()); // Cierra la ventana al presionarlo
+        JPanel panelBoton = new JPanel();
+        panelBoton.add(btnOK);
+        add(panelBoton, BorderLayout.SOUTH);
 
-        // Agregar la imagen al JFrame
-        add(labelImagen);
 
         // Ajustar tamaño automático al contenido
         pack();
